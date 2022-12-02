67e2ef5f45d55b9b86fc6dca21c743c3083cb464
Hisyam <hisyamskurniawan18@gmail.com>
2022-12-02 23:17:24 +0700

    feat: add lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,9 +3,28 @@
 ---------------------------------
 local lspconfig = require("lspconfig")
 local caps = vim.lsp.protocol.make_client_capabilities()
+
 local no_format = function(client, bufnr)
   client.resolved_capabilities.document_formatting = false
+
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 end
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Capabilities
 caps.textDocument.completion.completionItem.snippetSupport = true
